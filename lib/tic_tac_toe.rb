adcbf397d3d4e623ecf26727792d045c7f9625a8
AbdEl-Rahman21 <abdel292004@gmail.com>
2023-03-30 00:46:52 +0200

    Add one turn functionality

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -21,13 +21,13 @@ module Display
 
     puts "Symbol of Player ##{number}: "
 
-    gets player_info[:symbol] = gets.chomp
+    player_info[:symbol] = gets.chomp
 
     player_info
   end
 
   def create_board(array)
-    puts "== Open for business ==\n"
+    puts '== Open for business =='
     puts "\t #{array[0]} | #{array[1]} | #{array[2]} "
     puts "\t---+---+--- "
     puts "\t #{array[3]} | #{array[4]} | #{array[5]} "
@@ -38,36 +38,72 @@ module Display
   def winner_dec(number)
     put "Winner is Player ##{number}"
   end
+
+  def get_choice(player)
+    loop do
+      puts "#{player}, enter your choice: "
+
+      choice = gets.chomp.to_i
+
+      return choice if (1..9).include?(choice)
+
+      puts 'Error: Your choice must be form 1 to 9.'
+    end
+  end
 end
 
 class Game
   include Display
 
-  attr_accessor :tiles, :player_1, :player_2
+  attr_accessor :tiles, :player_1, :player_2, :winning_combo
 
   def initialize
     @tiles = Array(1..9)
     @winning_combo = LINES
-    @player_1
-    @player_2
   end
 
   def play
     create_players
+
+    play_turn(player_1)
   end
 
   def create_players
     create_board(tiles)
 
     info = get_player(1)
-    player_1 = Players.new(info[:name], info[:symbol])
+    @player_1 = Players.new(info[:name], info[:symbol])
 
     system('clear')
 
     create_board(tiles)
 
     info = get_player(2)
-    player_2 = Players.new(info[:name], info[:symbol])
+    @player_2 = Players.new(info[:name], info[:symbol])
+
+    system('clear')
+  end
+
+  def play_turn(player)
+    create_board(tiles)
+
+    loop do
+      choice = get_choice(player.name)
+
+      if tiles.include?(choice)
+        tiles.map! { |tile| tile == choice ? tile = player.symbol : tile }
+
+        winning_combo.each do |combo|
+          combo.map! do |element|
+            element == choice ? element = player.symbol : element
+          end
+        end
+
+        break
+      else
+        puts 'Error: Tile already picked.'
+      end
+    end
 
     system('clear')
   end
